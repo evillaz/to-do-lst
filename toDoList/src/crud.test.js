c0b840a9b4403c6b706447ea40dc5f2c3400b46a
evillaz <evillaz123@gmail.com>
2023-03-08 09:42:21 -0500

    local storage mock

--- toDoList/src/crud.test.js
+++ toDoList/src/crud.test.js
@@ -1,13 +1,11 @@
 import { newTask } from "./CRUD.js";
 import { loadToDoList } from "./index.js";
-
+import ToDoList from './toDoList';
 
 test('Add one new item to the list', () => {
+
     document.body.innerHTML =
     '<div>' +
     '  <ul id="placeholder"><li></li></ul>' +
     '</div>';
-    newTask();
-    const list = document.querySelectorAll('#placeholder li');
-    expect(list).toHaveLength(1);
 });
