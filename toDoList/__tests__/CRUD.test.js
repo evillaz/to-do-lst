53bb420693af075dced7cb2023afee0b43a17888
baqar-abbas <baqar007@hotmail.com>
2023-03-09 23:46:22 +0500

    update status testing code updated

--- toDoList/__tests__/CRUD.test.js
+++ toDoList/__tests__/CRUD.test.js
@@ -53,7 +53,27 @@ describe('edit', () => {
   });
 });
 
+describe('update status', () => {
+  beforeEach(() => {
+    addTask('new task');
+    addTask('new task');
+  });
+  test('before changing status ', () => {
+    const toDoTasks = JSON.parse(localStorage.getItem('toDoList'));
+    const initialCompletedValue = toDoTasks[1].completed;
+    expect(initialCompletedValue).toBe(false);
+  });
+  test('after changing status', () => {
+    changeStatus(1);
+    const toDoTasks = JSON.parse(localStorage.getItem('toDoList'));
+    const finalCompletedValue = toDoTasks[1].completed;
+    expect(finalCompletedValue).toBe(true);
+  });
+});
+
 beforeEach(() => {
   localStorage.setItem('toDoList', JSON.stringify([]));
   document.body.innerHTML = '<ul id="placeholder"></ul>';
 });
+
+
