18a8660cca62ff3d8428dce274a9ac387e2a0f7d
fuadmoin <fuadamoin@gmail.com>
2023-03-09 14:39:25 +0300

    Test remove function

--- toDoList/src/addremove.test.js
+++ toDoList/src/addremove.test.js
@@ -25,6 +25,20 @@ describe('addTask', () => {
   });
 });
 
+describe('remove', () => {
+  test('initial number of li items shuld be 0', () => {
+    let initialLength = document.querySelectorAll('.task').length;
+    expect(initialLength).toBe(0);
+  });
+  test('adds a new task to the list', () => {
+    addTask('new task');
+    addTask('new task');
+    remove(1);
+    let finalLength = document.querySelectorAll('.task').length;
+    expect(finalLength).toBe(1);
+  });
+});
+
 beforeEach(() => {
   localStorage.setItem('toDoList', JSON.stringify([]));
   document.body.innerHTML = '<ul id="placeholder"></ul>';
