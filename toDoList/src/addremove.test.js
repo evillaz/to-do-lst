c583a874351aa36fc404f9c3ee2300d12aee75f7
baqar-abbas <baqar007@hotmail.com>
2023-03-09 16:37:54 +0500

    Testing add functionality

--- toDoList/src/addremove.test.js
+++ toDoList/src/addremove.test.js
@@ -11,6 +11,20 @@ const localStorageMock = (() => {
 
 Object.defineProperty(window, 'localStorage', { value: localStorageMock });
 
+describe('addTask', () => {
+  test('initial number of li items shuld be 0', () => {
+    let initialLength = document.querySelectorAll('.task').length;
+    expect(initialLength).toBe(0);
+  });
+  test('adds a new task to the list', () => {
+    addTask('new task');
+    addTask('new task');
+    addTask('new task');
+    let finalLength = document.querySelectorAll('.task').length;
+    expect(finalLength).toBe(3);
+  });
+});
+
 beforeEach(() => {
   localStorage.setItem('toDoList', JSON.stringify([]));
   document.body.innerHTML = '<ul id="placeholder"></ul>';
